8422ed80468ac853c3f6e79757af360760c71167
Michele Dolfi <dol@zurich.ibm.com>
2025-05-24 14:15:56 +0200

    add print debug
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

--- file_checker.cpp
+++ file_checker.cpp
@@ -1,5 +1,6 @@
 #include <filesystem>
 #include <string>
+#include <iostream>
 
 #ifdef _WIN32
 #include <locale>
@@ -8,11 +9,14 @@
 
 
 bool file_exists(const std::string& filename) {
+    std::cout << "Checking: " << filename << std::endl;
     return std::filesystem::exists(filename);
 }
 
 bool file_exists_fix(const std::string& filename) {
+    std::cout << "Checking (with fix): " << filename << std::endl;
 #ifdef _WIN32
+    std::cout << "Using WIN32 fix!" << std::endl;
     // Convert UTF-8 string to UTF-16 wstring
     std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
     std::wstring wide_filename = converter.from_bytes(filename);
